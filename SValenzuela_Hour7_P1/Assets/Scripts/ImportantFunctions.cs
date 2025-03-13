1c3fb01b4adc0ec1aa6e3b8b25183f91e78149d1
samvmg <vzla.samantha@gmail.com>
2025-03-12 17:28:26 -0700

    hour 7 part 1 scripting

---
+++ SValenzuela_Hour7_P1/Assets/Scripts/ImportantFunctions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ImportantFunctions : MonoBehaviour
+{
+  public int runSpeed;
+
+  // Start is called before the first frame update
+    void Start()
+    {
+        print ("Start runs before an object Updates");
+        
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print ("This is called once a frame");
+        
+    }
+
+}

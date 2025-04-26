01866e3ce7c75d255e99f67f5d37950facd6e3ca
samvmg <vzla.samantha@gmail.com>
2025-04-25 20:15:23 -0700

    hour 10 chaos ball

---
+++ SValenzuela_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+
+    /* 
+    1. Is the game too easy or hard? It's too easy, player input makes no impact on gameplay.
+    2. What would make it easier or harder? It would make it more challenging if the player controlled the movement of the balls.
+    3. What would give it a "wow" factor? Giving it a time counter.
+    4. What parts of the game are fun? What parts of the game are tedious? The movement and dynamics of the ball movement make it look fun; waiting for the balls to enter the goals on their own makes it tedious and boring.
+    5. The improvements I have made in-game include:
+        - Changing size and shape of player's bumper
+        - Try covering up water with other game objects around the border of the arena
+        - Try adding variations of the chaos balls
+        - Try changing the footsteps audio to something that makes more sense with the water
+
+
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SValenzuela_Hour10/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    public bool isSolved = false;
+    
+    void OnTriggerEnter (Collider collider)
+
+    {
+        GameObject collidedWith = collider.gameObject;
+        if(collidedWith.tag == gameObject.tag)
+
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy (collidedWith);
+    
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

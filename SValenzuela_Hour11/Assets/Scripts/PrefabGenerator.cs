1512bf603cb9f63c31c11eafbd9299f8e6023a7e
samvmg <vzla.samantha@gmail.com>
2025-05-02 22:17:37 -0700

    exercise 11 done

---
+++ SValenzuela_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Whenever we hit the B key we will generate a prefab at the position of the original prefab. Whenever we hit the space key, we will generate a 
+        // prefab at the position of the spawn object that this script is attached to
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+                Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}

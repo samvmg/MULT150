a65fb0ed574945c820696bf45f986196166fde4e
samvmg <vzla.samantha@gmail.com>
2025-05-07 19:56:04 -0700

    hour 14 exercises

---
+++ SValenzuela_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,9 @@
+/* For this exercise, I've done the following modifications:
+
+1. Changed the background image of the UI
+2. Changed the colors of the text and subtext
+3. Changed the style of the typeface to shaded, italics, and outlined
+4. Changed the button color
+5. Added some custom text specific to the game
+
+*/

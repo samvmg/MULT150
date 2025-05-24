060bacd6f9891bdbec9ea3496069e522168afea3
samvmg <vzla.samantha@gmail.com>
2025-05-23 22:56:51 -0700

    hour 20

---
+++ SValenzuela_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,28 @@
+/*
+
+Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+I think it has an acceptable amount of difficulty - it's not boring nor engaging enough to keep a player coming back for more than one sitting. It lies on the easier side
+because it becomes predictable really fast - the phase out power gives an extra layer of security to the player too. 
+
+What 2 changes would you do to make it easier or harder?
+Perhaps adding a variability to the obstacles where one type immediatly ends the game, or adding a power up that's significantly better but harder to get due to it's speed, 
+could improve its playability. The visual aspect of the game could be improved as well with more detail, and it goes without saying that sound effects would make the experience
+more engaging and rewarding.
+
+List 2 things that would give it a "wow" factor.
+Like I mentioned, sound design and perhaps a better visual. Maybe adding points so the player can unlock different characters would encourage players to keep playing and also
+rewards them by giving them the ability to customize their experience.
+
+What 2 parts of the game are fun? 
+The time-based aspect. The sense of urgency and immediate positive feedback for collecting power-ups is very satisfying. Also the fact that you can see the human character on the
+screen instead of it just being a simple object like a cube.
+
+What 2 parts of the game are tedious?
+The lack of variability and predictability. There's little rewards for playing more than a couple times.
+
+Changes made:
+1. Changed the light of the powerup object to blue instead of light yellow, as well as the speed of the particles and shape (to circle)
+2. Changed the material of the ground.
+3. Changed albedo of both prefabs.
+
+*/
